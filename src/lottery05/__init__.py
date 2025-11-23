b9549e591f478f1a166be6ec9a7f27d9b80e8697
jyami.kim <mor2222@naver.com>
2025-11-23 22:28:22 +0900

    feat1: ruff 수정, pre-commit-config 수정

--- src/lottery05/__init__.py
+++ src/lottery05/__init__.py
@@ -4,16 +4,16 @@ Lottery Game Package (IO 의존성 제거 버전)
 """
 
 from .game import (
-    is_valid_lotto_number,
-    generate_lotto_numbers,
-    read_user_numbers,
-    count_match,
-    get_rank,
-    print_result,
-    LOTTO_NUMBER_COUNT,
-    LOTTO_MIN_NUMBER,
     LOTTO_MAX_NUMBER,
+    LOTTO_MIN_NUMBER,
+    LOTTO_NUMBER_COUNT,
     RANK_BY_MATCH_COUNT,
+    count_match,
+    generate_lotto_numbers,
+    get_rank,
+    is_valid_lotto_number,
+    print_result,
+    read_user_numbers,
 )
 
 __all__ = [
